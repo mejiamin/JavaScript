51431c4ce1edad5a7ae6d4527f0eb1cd8337ad3c
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-08 12:30:23 +0300

    Managed loop in tasks in simple

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 
 // script.js
 
+let startNumber = 1;
+let multiplier = 4;
+let quantity = 7;
+
+for (let i = 0; i < quantity; i++) {
+    console.log(startNumber);
+    startNumber *= multiplier;
+}
