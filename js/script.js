267f637a624dfe336e4f463a0fe2d7ffc36256ed
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-23 07:18:46 +0300

    Again

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 
 // script.js
 
-let price = 4000;
-let discountedPrice;
+let time = 15;
 
-if (1000 <= price && price < 3000) {
-    discountedPrice = price - (price * 5 / 100);
-} else if (3000 <= price && price < 5000) {
-    discountedPrice = price - (price * 10 / 100);
-} else if (5000 <= price) {
-    discountedPrice = price - (price * 15 / 100);
-} else {
-    discountedPrice = price;
+let goToDairy = false;      // На молокозавод
+let goToStore = false;      // В магазин
+let goToMarket = false;     // Н рынок
+
+let messageDairy
+
+if ((8 <= time && time < 13) || (14 <= time && time < 19)) {
+    goToDairy = true;
+} else if ((9 <= time && time < 14) || (15 <= time && time < 17)) {
+    goToStore = true;
+} else if (7 <= time && time < 20) {
+    goToMarket = true;
 }
 
-console.log(discountedPrice);
+console.log(goToDairy);
+console.log(goToStore);
+console.log(goToMarket);
